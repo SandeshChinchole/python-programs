06de1b981bda8342856a77fa6ff21c16973f7e28
Sandesh Chinchole <sc31285n@pace.edu>
2021-01-06 23:33:36 -0500

    added files for directory 5

--- 5/bark.py
+++ 5/bark.py
@@ -1,15 +1,10 @@
-
-print('Get those dogs ready')
-
-def bark(dog_name, dog_weight):
-    if dog_weight > 20:
-        print(dog_name, 'says WOOF WOOF')
+def bark(name, weight):
+    if weight > 20:
+        print(name, 'says WOOF WOOF')
     else:
-        print(dog_name, 'says woof woof')
+        print(name, 'says woof woof')
 
-bark('codie', 40)
+bark('Codie', 40)
 bark('Sparky', 9)
 bark('Jackson', 12)
 bark('Fido', 65)
-
-print("Okay, we're all done here")
